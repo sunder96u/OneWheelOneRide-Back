997f3f740fdad0891d3b8453bdc799a02aa1fe09
Steven Underwood <stevenunderwood2@hotmail.com>
2023-07-25 14:49:39 -0500

    change group model

--- onewheeloneride/models.py
+++ onewheeloneride/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class User(models.Model):
     id = models.AutoField(primary_key=True)
     email = models.CharField(max_length=100)
-    user_name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
@@ -12,7 +12,7 @@ class User(models.Model):
     phone_number = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.user_name
+        return self.name
     
 class Category(models.Model):
     id = models.AutoField(primary_key=True)
@@ -90,7 +90,7 @@ class Group(models.Model):
     name = models.CharField(max_length=100)
     picture = models.CharField(max_length=250)
     description = models.TextField(default='No Description Given')
-    user_id = models.ManyToManyField('self', through='User')
+    members = models.ManyToManyField(User, blank=True)
 
     def __str__(self):
         return self.name
